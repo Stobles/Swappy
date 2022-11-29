c7ab09d87e42f5fdc865258f3c526767ceeb9111
Stobles <82175280+Stobles@users.noreply.github.com>
2022-11-29 21:57:48 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,10 +1,13 @@
 import { Select } from "./modules/select.js";
+import { sidebar } from "./modules/sidebar.js";
 
 const headerSelect = new Select('.header__select', {
   defaultId: 2,
   data: [
     {id: 1, src: 'img/header/Select/united-kingdom.png', value: 'English/USD'},
     {id: 2, src: 'img/header/Select/russia.png', value: 'Русский/РУБ'},
-    {id: 3, src: 'img/header/Select/Germany.png', value: 'Germany/EUR'}
+    {id: 3, src: 'img/header/Select/germany.png', value: 'Germany/EUR'}
   ]
 })
+
+sidebar('#header-sidebar','#burger-open', '#burger-close');
--- js/modules/sidebar.js
+++ js/modules/sidebar.js
@@ -1,5 +1,15 @@
-export class Sidebar{
-  constructor(selector){
-    this.selector = selector;
-  }
+export function sidebar(selector, openSelector, closeSelector) {
+  const sidebar = document.querySelector(selector);
+  const openButton = document.querySelector(openSelector);
+  const closeButton = document.querySelector(closeSelector);
+
+  openButton.addEventListener('click', () => {
+    document.body.classList.add('lock');
+    sidebar.classList.add('active');
+  });
+
+  closeButton.addEventListener('click', () => {
+    document.body.classList.remove('lock');
+    sidebar.classList.remove('active');
+  });
 }
